84b298ff19f8e5b6793b5e45fa082cd4f0f3a6dc
“Steve <steveshackleton@hotmail.co.uk>
2020-10-16 13:19:24 +0100

    login-page-first-commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,9 +4,14 @@ import Styles from './Login.module.scss';
 const Login = () => {
     return (
         <div className={Styles.loginPage}>
-            <h1 className={Styles.title}>Hi welcome to Login Page</h1>
-            <input />
-            <button>Login</button>
+            <h1 className={Styles.title}>Shift Reporter Portal</h1>
+            <label className= {Styles.idnumber}>ID Number:</label><br></br>
+            <input  placeholder="i.e 763489" required/>
+            <br></br>
+            <label className={Styles.password}>Password:</label><br></br>
+            <input placeholder="******" required/>
+            <br></br>
+            <button className={Styles.submit} value="login">Login</button>
         </div>
     )
 }
